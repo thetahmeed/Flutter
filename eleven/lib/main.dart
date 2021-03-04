0f4fbb6ef4a9dad98bf5432a262a84d07c1c6460
thetahmeed <tahmeed.com.bd@gmail.com>
2021-03-04 14:26:26 +0600

    Container

--- eleven/lib/main.dart
+++ eleven/lib/main.dart
@@ -20,16 +20,16 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("AppBar Title"),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Text("Body Text", 
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Colors.red,
-          fontSize: 30,
-          fontStyle: FontStyle.italic
+      body: Container(
+        margin: EdgeInsets.only(left: 12),
+        padding: EdgeInsets.all(12),
+        height: 80,
+        width: 120,
+        decoration: BoxDecoration(
+          color: Colors.indigo,
+          borderRadius: BorderRadius.circular(10)
           ),
-        ),
+        child: Text("Text on container"),
       )
     );
   }
