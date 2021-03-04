168f3ba7c3472f0e4616e9ebf689e452eda462f8
thetahmeed <tahmeed.com.bd@gmail.com>
2021-03-04 21:55:16 +0600

    Image

--- eleven/lib/main.dart
+++ eleven/lib/main.dart
@@ -1,9 +1,7 @@
-
-
-
-
-
 import 'package:flutter/material.dart';
+
+final _longText =
+    "Trust me or not but this is a very very very very long text. Not very big. But big";
 
 void main() {
   runApp(MaterialApp(
@@ -16,36 +14,42 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("AppBar Title"),
-      ),
-      body: Column(
-        children: [
-          Container(
-        margin: EdgeInsets.only(top: 12),
-        padding: EdgeInsets.all(12),
-        height: 80,
-        width: 100,
-        child: Icon(Icons.warning),
-        decoration: BoxDecoration(
-          color: Colors.indigo,
-          borderRadius: BorderRadius.circular(10)
-          ),
-      ),
-      Container(
-        margin: EdgeInsets.only(top: 12),
-        padding: EdgeInsets.all(12),
-        height: 80,
-        width: 100,
-        foregroundDecoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage("https://fujifilm-x.com/wp-content/uploads/2019/08/x-t30_sample-images02.jpg"),
-            )
+        appBar: AppBar(
+          title: Text("AppBar Title"),
         ),
-        child: Text("Text on container"),
-      ),
-      ],
-     )
-    );
+        body: Column(
+          children: [
+
+            Image.asset("assets/images/guns.png"), 
+
+            Container(
+              child: Text(_longText),
+              padding: EdgeInsets.all(10),
+            ),
+
+            Container(
+              margin: EdgeInsets.only(top: 12),
+              padding: EdgeInsets.all(12),
+              height: 80,
+              width: 100,
+              child: Icon(Icons.warning),
+              decoration: BoxDecoration(
+                  color: Colors.indigo,
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 12),
+              padding: EdgeInsets.all(12),
+              height: 80,
+              width: 100,
+              foregroundDecoration: BoxDecoration(
+                  image: DecorationImage(
+                image: NetworkImage(
+                    "https://fujifilm-x.com/wp-content/uploads/2019/08/x-t30_sample-images02.jpg"),
+              )),
+              child: Text("Text on container"),
+            ),
+          ],
+        ));
   }
 }
