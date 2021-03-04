b7ce9d7eb69027c3f35fb24a65d36cba430c06ee
thetahmeed <tahmeed.com.bd@gmail.com>
2021-03-04 12:00:27 +0600

    Simple First App 2

--- eleven/lib/main.dart
+++ eleven/lib/main.dart
@@ -6,22 +6,21 @@
 
 import 'package:flutter/material.dart';
 
-void main() {                                       // 1. start with main function
-  runApp(MyApp());                                  // 2. runApp with a starting class
+void main() {
+  runApp(MaterialApp(
+    title: "Eleven",
+    home: HomePage(),
+  ));
 }
 
-class MyApp extends StatelessWidget {               // 3. here MyApp is a stateless wedget so we have to override buid method
-  
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(                             // 4. returning MaterialApp
-      title: 'Flutter Demo',
-      home: Scaffold(                               // 5. scaffold for home page design
-        appBar: AppBar(
-          title: Text("AppBar Title"),
-          ),
-        body: Text("BodyText"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("AppBar Title"),
       ),
+      body: Text("Body Text"),
     );
   }
 }
