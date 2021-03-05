f202ad3595a7debce1440fef2897f80660265a2e
thetahmeed <tahmeed.com.bd@gmail.com>
2021-03-05 21:23:18 +0600

    List

--- eleven/lib/main.dart
+++ eleven/lib/main.dart
@@ -3,7 +3,22 @@ import 'package:flutter/material.dart';
 final _longText =
     "Trust me or not\n\n\n but this is a very very\n\n very very long text.\n\n\n Not very big. But big";
 
-Widget _makeAGap(){
+final List _myList = [
+  {
+    "name" : "Tahmeed",
+    "age" : "20"
+  },
+  {
+    "name" : "Another Tahmeed",
+    "age" : "40"
+  },
+  {
+    "name" : "Again Another Tahmeed",
+    "age" : "30"
+  }
+];
+
+Widget _makeAGap() {
   return SizedBox(
     width: 10,
   );
@@ -35,43 +50,6 @@ class HomePage extends StatelessWidget {
                   fontSize: 30,
                   fontWeight: FontWeight.bold),
             ),
-            SizedBox(
-              width: 120,
-              height: 120,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.pink,
-                ),
-              ),
-            ),
-            Row(
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(color: Colors.red),
-                ),
-
-                _makeAGap(),
-
-                Expanded(
-                  child: Container(
-                    child: Icon(Icons.face, color: Colors.white,),
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(color: Colors.green),
-                  ),
-                ),
-
-                _makeAGap(),
-
-                Container(
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(color: Colors.purple),
-                ),
-              ],
-            )
           ],
         ));
   }
