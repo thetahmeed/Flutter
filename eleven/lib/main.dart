0385ae1d69c70df7a8476db53b4f02d9645efb26
thetahmeed <tahmeed.com.bd@gmail.com>
2021-03-06 21:16:38 +0600

    Read Local Json File

--- eleven/lib/main.dart
+++ eleven/lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
-Widget _makeAGap() {
-  return SizedBox(
-    width: 10,
-  );
-}
+import 'dart:async' show Future;
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:convert';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -20,97 +18,80 @@ class HomePage2 extends StatefulWidget {
 }
 
 class _HomePage2State extends State<HomePage2> {
+  List data;
 
-  _showToast(){
-    Fluttertoast.showToast(
-        msg: "This is Center Short Toast",
-        toastLength: Toast.LENGTH_LONG,
-        gravity: ToastGravity.CENTER,
-        timeInSecForIosWeb: 1,
-        backgroundColor: Colors.red,
-        textColor: Colors.white,
-        fontSize: 16.0
-    );
-    /*
-    Custom Toast will not work on android 11 and above, 
-    it will only use msg and toastLength remaining 
-    all properties are ignored
-    */
+  Future<String> loadJsonData() async {
+    var jsonText = await rootBundle.loadString('assets/data.json');
+    setState(() => data = json.decode(jsonText));
+    print(jsonText);
+  }
 
+  @override
+  void initState() {
+    super.initState();
+    this.loadJsonData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      appBar: AppBar(
-        title: Text("AppBar Title"),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            Container(
-              height: 200,
-              decoration: BoxDecoration(color: Colors.pinkAccent),
-              child: Center(
-                child: Text(
-                  "Menu",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
+        appBar: AppBar(
+          title: Text("AppBar Title"),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              Container(
+                height: 200,
+                decoration: BoxDecoration(color: Colors.pinkAccent),
+                child: Center(
+                  child: Text(
+                    "Menu",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
                 ),
               ),
-            ),
-            ListTile(
-              leading: Icon(Icons.face),
-              title: Text("Profile"),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.mail),
-              title: Text("Mail"),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.build),
-              title: Text("Setting"),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.exit_to_app),
-              title: Text("Exit"),
-              onTap: () {},
-            ),
-          ],
+              ListTile(
+                leading: Icon(Icons.face),
+                title: Text("Profile"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.mail),
+                title: Text("Mail"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.build),
+                title: Text("Setting"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.exit_to_app),
+                title: Text("Exit"),
+                onTap: () {},
+              ),
+            ],
+          ),
         ),
-      ),
-
-      body: Column(
-        children: [
-          ElevatedButton(
-            child: Text("Show Toast"),
-            onPressed: () {
-              setState(() {
-                _showToast();
-              });
-            },
-          ),
-          ElevatedButton(
-            child: Text("Show Snack"),
-            onPressed: () {
-              setState(() {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: const Text('A SnackBar has been shown.'),
-                  ),
-                );
-              });
-            },
-          ),
-        ],
-      ),
+        body: ListView.builder(
+          itemCount: data == null ? 0 : data.length,
+          itemBuilder: (BuildContext contex, int index) {
+            return Column(
+              children: [
+                ListTile(
+                  leading: CircleAvatar(child: Text(data[index]["name"][0]),),
+                  title: Text(data[index]["name"]),
+                  subtitle: Text(data[index]["city"]),
+                )
+              ],
+            );
+          },
+        )
     );
   }
 }
