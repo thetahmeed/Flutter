e3b559be2dddf0e31ea75c5f00f545083c700e71
thetahmeed <tahmeed.com.bd@gmail.com>
2021-03-08 15:34:03 +0600

    SearchView in listview

--- twelve/lib/main.dart
+++ twelve/lib/main.dart
@@ -18,19 +18,47 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   List _myList;
+  List _unFiltered;
+
   Future<bool> _getPosts() async {
     // https://jsonplaceholder.typicode.com/posts (The main URL)
+    // https://api.mocki.io/v1/192ea43d
 
-    var response = await http.get(Uri.https('jsonplaceholder.typicode.com', '/posts'));
+    var response =
+        await http.get(Uri.https('api.mocki.io', '/v1/192ea43d'));
 
     setState(() {
       _myList = convert.jsonDecode(response.body);
+      this._unFiltered = _myList;
     });
   }
 
   @override
   void initState() {
     this._getPosts();
+  }
+
+  searchData(String key) {
+    var dataExistOrNot = key.length > 0 ? true : false;
+
+    if (dataExistOrNot) {
+      var _filtered = [];
+
+      for(var i = 0; i< this._unFiltered.length; i++){
+        if(_unFiltered[i]['title'].toString().toLowerCase().contains(key.toLowerCase())){
+          _filtered.add(_unFiltered[i]);
+        }
+      }
+
+      setState(() {
+        this._myList = _filtered;
+      });
+
+    } else {
+      setState(() {
+        this._myList = this._unFiltered;
+      });
+    }
   }
 
   @override
@@ -40,20 +68,37 @@ class _DashboardState extends State<Dashboard> {
         centerTitle: true,
         title: Text("Dashboard"),
       ),
-      body: ListView.builder(
-        itemCount: _myList == null ? 0 : _myList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            leading: CircleAvatar(child: Icon(Icons.book),),
-            title: Text(_myList[index]["title"]),
-            subtitle: Text(_myList[index]["body"]),
-            onTap: () {
-              Route route = MaterialPageRoute(
-                  builder: (context) => PageOne(_myList[index]));
-              Navigator.push(context, route);
-            },
-          );
-        },
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: TextField(
+              decoration: InputDecoration(hintText: "Search..."),
+              onChanged: (String key) {
+                this.searchData(key);
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _myList == null ? 0 : _myList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    child: Icon(Icons.book),
+                  ),
+                  title: Text(_myList[index]["title"]),
+                  subtitle: Text(_myList[index]["body"]),
+                  onTap: () {
+                    Route route = MaterialPageRoute(
+                        builder: (context) => PageOne(_myList[index]));
+                    Navigator.push(context, route);
+                  },
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
