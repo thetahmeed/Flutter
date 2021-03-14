f5ac3731c3dc3a2a3a8736169f89d30fd19661ab
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-03-14 18:06:44 +0600

    IGNORE: Added a new page called menu.dart

--- fourteen/lib/main.dart
+++ fourteen/lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:fourteen/pages/form.dart';
 import 'package:fourteen/pages/home.dart';
+import 'package:fourteen/pages/menu.dart';
 
 void main() {
   runApp(
     MaterialApp(
       title: "MatApp",
       //home: HomePage(),
-      home: FormPage(),
+      //home: FormPage(),
+      home: MenuPage(),
       debugShowCheckedModeBanner: false,
     ),
   );
---
+++ fourteen/lib/pages/menu.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MenuPage extends StatefulWidget {
+  @override
+  _MenuPageState createState() => _MenuPageState();
+}
+
+class _MenuPageState extends State<MenuPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Menu Page"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text("Show me the menu"),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.blueAccent,
+        shape: const CircularNotchedRectangle(),
+        child: Container(
+          height: 50.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() {}),
+        tooltip: 'Hint Text',
+        child: Text("T"),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
