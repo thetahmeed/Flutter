f28e200fe81eae71248a96a47f33949e321ac800
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-03-14 20:38:24 +0600

    PopupButton, onSelected

--- fourteen/lib/pages/menu.dart
+++ fourteen/lib/pages/menu.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:fourteen/models/user.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MenuPage extends StatefulWidget {
   @override
@@ -14,7 +15,10 @@ class _MenuPageState extends State<MenuPage> {
   List users = [];
 
   _loadData() async {
-    var jsonString = await rootBundle.loadString("data.json");
+    // When request is from Android
+    var jsonString = await rootBundle.loadString("assets/data.json");
+    // When request is from Web
+
     setState(() {
       this.users = json.decode(jsonString);
     });
@@ -24,6 +28,77 @@ class _MenuPageState extends State<MenuPage> {
   void initState() {
     super.initState();
     _loadData();
+  }
+
+  void _launchURL(var _url) async {
+    await canLaunch(_url) ? await launch(_url) : throw 'Could not launch $_url';
+  }
+
+  void _callOrSmsUser(var _phone) async {
+    await canLaunch(_phone)
+        ? await launch(_phone)
+        : throw 'Could not launch $_phone';
+  }
+
+  listPopUpMenu(User user) {
+    return PopupMenuButton(
+      offset: Offset(0, 40),
+      onSelected: (value) {
+        switch (value) {
+          case "call":
+            _callOrSmsUser("tel:${user.employeeSalary}");
+            break;
+
+          case "message":
+            _callOrSmsUser("sms:${user.employeeSalary}");
+            break;
+
+          case "website":
+            _launchURL("https://pub.dev/packages/url_launcher");
+            break;
+        }
+      },
+      itemBuilder: (BuildContext context) {
+        return [
+          PopupMenuItem(
+            child: Row(
+              children: [
+                Icon(Icons.call),
+                SizedBox(
+                  width: 10,
+                ),
+                Text("Call"),
+              ],
+            ),
+            value: "call",
+          ),
+          PopupMenuItem(
+            child: Row(
+              children: [
+                Icon(Icons.message),
+                SizedBox(
+                  width: 10,
+                ),
+                Text("Send message"),
+              ],
+            ),
+            value: "message",
+          ),
+          PopupMenuItem(
+            child: Row(
+              children: [
+                Icon(Icons.web),
+                SizedBox(
+                  width: 10,
+                ),
+                Text("Visit website"),
+              ],
+            ),
+            value: "website",
+          )
+        ];
+      },
+    );
   }
 
   @override
@@ -49,6 +124,10 @@ class _MenuPageState extends State<MenuPage> {
                   child: Text("About"),
                   value: "about",
                 ),
+                PopupMenuItem(
+                  child: Text("Website"),
+                  value: "website",
+                ),
               ];
             },
           )
@@ -71,6 +150,7 @@ class _MenuPageState extends State<MenuPage> {
               ),
               title: Text(singleUser.employeeName),
               subtitle: Text("Age: " + singleUser.employeeAge.toString()),
+              trailing: listPopUpMenu(singleUser),
               onTap: () {},
             );
           },
