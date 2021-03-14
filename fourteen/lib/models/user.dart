6f84a8e5fa481d4e0137ab60decf5a2d46f2c63e
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-03-14 18:07:05 +0600

    IGNORE: Created a mode class

---
+++ fourteen/lib/models/user.dart
@@ -0,0 +1,18 @@
+class User {
+  int id;
+  String employeeName;
+  int employeeSalary;
+  int employeeAge;
+  String profileImage;
+
+  User(this.id, this.employeeName, this.employeeSalary, this.employeeAge,
+      this.profileImage);
+
+  User.fromJson(dynamic json) {
+    id = json["id"];
+    employeeName = json["employee_name"];
+    employeeSalary = json["employee_salary"];
+    employeeAge = json["employee_age"];
+    profileImage = json["profile_image"];
+  }
+}
