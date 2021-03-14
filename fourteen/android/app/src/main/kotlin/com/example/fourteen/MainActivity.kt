7f056bc08d40132e45b4f666c4418579efcb1b24
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-03-14 11:45:19 +0600

    IGNORE: Made an app with a home page

---
+++ fourteen/android/app/src/main/kotlin/com/example/fourteen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fourteen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ fourteen/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:fourteen/pages/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "MatApp",
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
