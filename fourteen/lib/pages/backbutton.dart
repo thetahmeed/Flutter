e9326c22c3643ed7b0ac328e8aabede5e315425d
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-03-16 22:02:02 +0600

    Exit Dialogue

--- fourteen/lib/pages/backbutton.dart
+++ fourteen/lib/pages/backbutton.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class BackButtonPopUp extends StatefulWidget {
   @override
@@ -6,18 +7,49 @@ class BackButtonPopUp extends StatefulWidget {
 }
 
 class _BackButtonState extends State<BackButtonPopUp> {
+  _showExitPopUp() {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Exit"),
+          content: Text("Do you want to exit?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("No"),
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.purpleAccent),
+              ),
+              onPressed: () {
+                SystemNavigator.pop();
+              },
+              child: Text("Yes"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("BackButton"),
-            centerTitle: true,
-            backgroundColor: Colors.purpleAccent,
-          ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("BackButton"),
+          centerTitle: true,
+          backgroundColor: Colors.purpleAccent,
         ),
-        onWillPop: () {
-          showDialog(context: context, builder: (BuildContext context){} )
-        });
+      ),
+      onWillPop: () {
+        _showExitPopUp();
+      },
+    );
   }
 }
