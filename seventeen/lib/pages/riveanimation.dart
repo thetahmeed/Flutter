e86b1e13070958fb5f5d4a3681189fbb0ddb649c
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-03-24 23:29:44 +0600

    RiveAnimation Start

---
+++ seventeen/lib/pages/riveanimation.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RiveAnimationPrac extends StatefulWidget {
+  @override
+  _RiveAnimationPracState createState() => _RiveAnimationPracState();
+}
+
+class _RiveAnimationPracState extends State<RiveAnimationPrac> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Rive"),
+        centerTitle: true,
+      ),
+      body: Text('sdggs'),
+    );
+  }
+}
