259bf04c9e7d0aff5358a9eed4897d3e36b2a842
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-04-14 12:59:52 +0600

    AnimatedPadding

--- nineteen/lib/pages/things.dart
+++ nineteen/lib/pages/things.dart
@@ -7,37 +7,44 @@ class LittleThings extends StatefulWidget {
 
 class _LittleThingsState extends State<LittleThings> {
   int _counter = 0;
+  double _paddingValue = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            AnimatedSwitcher(
-              duration: const Duration(milliseconds: 300),
-              transitionBuilder: (Widget child, Animation<double> animation) {
-                return ScaleTransition(child: child, scale: animation);
-              },
-              child: Text(
-                '' + _counter.toString(),
-                style: TextStyle(fontSize: 26),
-                key: ValueKey<int>(_counter),
+        child: AnimatedPadding(
+          padding: EdgeInsets.all(_paddingValue),
+          duration: Duration(milliseconds: 300),
+          curve: Curves.easeIn,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: (Widget child, Animation<double> animation) {
+                  return ScaleTransition(child: child, scale: animation);
+                },
+                child: Text(
+                  '' + _counter.toString(),
+                  style: TextStyle(fontSize: 26),
+                  key: ValueKey<int>(_counter),
+                ),
               ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _counter++;
-                    });
-                  },
-                  child: Text('Click')),
-            )
-          ],
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _counter++;
+                        _paddingValue = 18.0;
+                      });
+                    },
+                    child: Text('Click')),
+              )
+            ],
+          ),
         ),
       ),
     );
