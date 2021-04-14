d087a818c2e451598c32c75b72ed2b32792d6253
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-04-14 12:46:15 +0600

    AnimatedSwitcher

--- nineteen/lib/pages/things.dart
+++ nineteen/lib/pages/things.dart
@@ -6,37 +6,38 @@ class LittleThings extends StatefulWidget {
 }
 
 class _LittleThingsState extends State<LittleThings> {
-  final List<int> _items = List<int>.generate(10, (int index) => index);
+  int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
-    final ColorScheme colorScheme = Theme.of(context).colorScheme;
-    final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
-    final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
-
     return Scaffold(
       body: SafeArea(
-        child: ReorderableListView(
-          padding: EdgeInsets.all(8),
-          children: <Widget>[
-            for (int index = 0; index < _items.length; index++)
-              ListTile(
-                key: Key('$index'),
-                tileColor: _items[index].isOdd ? oddItemColor : evenItemColor,
-                title: Text('Item ${_items[index]}'),
-              ),
-          ],
-          onReorder: (int oldIndex, int newIndex) {
-            setState(
-              () {
-                if (oldIndex < newIndex) {
-                  newIndex -= 1;
-                }
-                final int item = _items.removeAt(oldIndex);
-                _items.insert(newIndex, item);
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                return ScaleTransition(child: child, scale: animation);
               },
-            );
-          },
+              child: Text(
+                '' + _counter.toString(),
+                style: TextStyle(fontSize: 26),
+                key: ValueKey<int>(_counter),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _counter++;
+                    });
+                  },
+                  child: Text('Click')),
+            )
+          ],
         ),
       ),
     );
