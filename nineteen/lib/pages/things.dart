082b5c8054ab2cdd00316f07753226f983a34004
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-04-14 12:13:02 +0600

    Placeholder

--- nineteen/lib/pages/things.dart
+++ nineteen/lib/pages/things.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class LittleThings extends StatefulWidget {
@@ -9,40 +7,16 @@ class LittleThings extends StatefulWidget {
 
 class _LittleThingsState extends State<LittleThings>
     with TickerProviderStateMixin {
-  AnimationController _controller;
-  Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = AnimationController(
-        duration: const Duration(milliseconds: 5000),
-        vsync: this,
-        value: 0,
-        lowerBound: 0,
-        upperBound: 1);
-    _animation =
-        CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn);
-
-    _controller.forward();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Row(
-            children: [
-              Container(color: Colors.red, height: 100, width: 100),
-              Spacer(flex: 2),
-              Container(color: Colors.green, height: 100, width: 100),
-              Spacer(flex: 1),
-              Container(color: Colors.blue, height: 100, width: 100),
-            ],
-          ),
-        ),
+            child: Container(
+          height: 100,
+          width: 200,
+          child: Placeholder(),
+        )),
       ),
     );
   }
