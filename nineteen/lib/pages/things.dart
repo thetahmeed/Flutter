9d8dd7b2bc1f6bdeaa3117d4053d468a84d913b5
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-04-15 13:49:35 +0600

    FractionallySizedBox

--- nineteen/lib/pages/things.dart
+++ nineteen/lib/pages/things.dart
@@ -17,32 +17,19 @@ class _LittleThingsState extends State<LittleThings> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              ConstrainedBox(
-                constraints: BoxConstraints(maxWidth: 200),
-                child: Text('I am not in danger, I am the danger - Heisenberg'),
-              ),
-              ConstrainedBox(
-                constraints: BoxConstraints(maxWidth: 200),
-                child: AnimatedOpacity(
-                  opacity: _opacityControl,
-                  duration: Duration(milliseconds: 500),
-                  child: Container(height: 100, color: Colors.red),
-                ),
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _opacityControl == 0
-                          ? _opacityControl = 1
-                          : _opacityControl = 0;
-                    });
-                  },
-                  child: Text('Click to visible the box'))
-            ],
-          ),
+        child: Column(
+          children: [
+            FractionallySizedBox(
+              alignment: Alignment.center,
+              widthFactor: 0.6,
+              child: ElevatedButton(onPressed: () {}, child: Text('abc')),
+            ),
+            FractionallySizedBox(
+              alignment: Alignment.center,
+              widthFactor: 0.8,
+              child: ElevatedButton(onPressed: () {}, child: Text('abc')),
+            ),
+          ],
         ),
       ),
     );
