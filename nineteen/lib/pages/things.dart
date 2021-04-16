8d357ccd6a237da567df1d12f3def552b7075290
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-04-16 15:15:33 +0600

    CheckboxListTile

--- nineteen/lib/pages/things.dart
+++ nineteen/lib/pages/things.dart
@@ -7,14 +7,41 @@ class LittleThings extends StatefulWidget {
 }
 
 class _LittleThingsState extends State<LittleThings> {
+  bool _checked1 = false;
+  bool _checked2 = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: CupertinoActivityIndicator(
-            animating: true,
-            radius: 50,
+          child: ListView(
+            children: [
+              CheckboxListTile(
+                title: Text('Title'),
+                subtitle: Text('Subtitle'),
+                secondary: Icon(Icons.more_vert),
+                controlAffinity: ListTileControlAffinity.leading,
+                value: _checked1,
+                onChanged: (v) {
+                  setState(() {
+                    _checked1 = v;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                title: Text('Title'),
+                subtitle: Text('Subtitle'),
+                secondary: Icon(Icons.more_vert),
+                controlAffinity: ListTileControlAffinity.leading,
+                value: _checked2,
+                onChanged: (v) {
+                  setState(() {
+                    _checked2 = v;
+                  });
+                },
+              ),
+            ],
           ),
         ),
       ),
