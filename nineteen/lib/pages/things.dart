825e8b7546468f7ca3c05ffe002bc19519fbec38
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-04-16 13:57:52 +0600

    CupertinoActionSheet

--- nineteen/lib/pages/things.dart
+++ nineteen/lib/pages/things.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LittleThings extends StatefulWidget {
@@ -12,26 +13,38 @@ class _LittleThingsState extends State<LittleThings> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: ToggleButtons(
-            children: [
-              Icon(Icons.camera_rounded),
-              Icon(Icons.photo_size_select_actual_rounded),
-              Icon(Icons.info)
-            ],
-            isSelected: _selections,
-            onPressed: (i) {
-              setState(() {
-                _selections[i] = !_selections[i];
-              });
-            },
-            color: Colors.grey,
-            selectedBorderColor: Colors.pink,
-            selectedColor: Colors.pink,
-            borderRadius: BorderRadius.all(Radius.circular(15)),
-          ),
+        child: CupertinoButton(
+          onPressed: () {
+            showCupertinoModalPopup(
+                context: context, builder: (context) => _theSheet(context));
+          },
+          child: Text('Click here'),
         ),
       ),
+    );
+  }
+
+  Widget _theSheet(BuildContext context) {
+    return CupertinoActionSheet(
+      title: Text('Title'),
+      message: Text('This is a message'),
+      actions: [
+        CupertinoActionSheetAction(
+          onPressed: () {},
+          child: Text('Data1'),
+          isDefaultAction: true,
+        ),
+        CupertinoActionSheetAction(
+          onPressed: () {},
+          child: Text('Data1'),
+          isDestructiveAction: true,
+        ),
+      ],
+      cancelButton: CupertinoActionSheetAction(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Cancel')),
     );
   }
 }
