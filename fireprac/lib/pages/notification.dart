81e4b52bcbc078766bee54386b93a60ee514ff9f
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-04-27 15:32:44 +0600

    4. Open a specific route/activity by clicking on notification

--- fireprac/lib/pages/notification.dart
+++ fireprac/lib/pages/notification.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
-
-
 class NotificationTestPage extends StatefulWidget {
   @override
   _NotificationTestPageState createState() => _NotificationTestPageState();
@@ -24,7 +22,11 @@ class _NotificationTestPageState extends State<NotificationTestPage> {
       print('Body: ' + event.notification.body);
     });
 
-    
+    // Open a specific route/activity by clicking on notification
+    FirebaseMessaging.onMessageOpenedApp.listen((event) {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => AllNotificationPage()));
+    });
   }
 
   @override
@@ -33,6 +35,19 @@ class _NotificationTestPageState extends State<NotificationTestPage> {
       body: Center(
         child: Text('This is a data'),
       ),
+    );
+  }
+}
+
+class AllNotificationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('All noitifications'),
+        centerTitle: true,
+      ),
+      body: Center(child: Text('Empty')),
     );
   }
 }
