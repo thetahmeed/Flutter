2affd67ec9cf371eaa7a75d9104942141c36cfcb
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-04-27 14:48:42 +0600

    1. Get token

--- fireprac/lib/pages/notification.dart
+++ fireprac/lib/pages/notification.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-class NotificationTestPage extends StatelessWidget {
+class NotificationTestPage extends StatefulWidget {
+  @override
+  _NotificationTestPageState createState() => _NotificationTestPageState();
+}
+
+class _NotificationTestPageState extends State<NotificationTestPage> {
+  var fbm = FirebaseMessaging.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    fbm.getToken().then((value) => print('The token $value'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
