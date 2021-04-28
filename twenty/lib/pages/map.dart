35150c198b40b11dfcae9d7967f229ce9b39aa34
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-04-28 14:12:58 +0600

    2. Get the API key

--- twenty/lib/pages/map.dart
+++ twenty/lib/pages/map.dart
@@ -20,3 +20,12 @@ class _MyMapPageState extends State<MyMapPage> {
     );
   }
 }
+
+
+/*
+Get the API:
+1. Go to this link and create a project > https://cloud.google.com/maps-platform/
+2. Open navigation drawer and select > Google Maps Platform > Map SDK for android > Enable
+3. APIs > Maps SDK for iOS > Enable
+4. Credentials > Credentials in APIs & Services > Create credentials > API key > Copy the key
+*/
